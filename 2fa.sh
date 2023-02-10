693f3a417f400e160bf8484d030792790171d287
Am0rphous <29645801+Am0rphous@users.noreply.github.com>
2023-02-11 00:24:45 +0100

    Update 2fa.sh

--- 2fa.sh
+++ 2fa.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
+#credit: github.com/Am0rphous
+#09.02.23
 
 USER="$1"
 
 TOKEN="your secret token here"
 URL="example.gitlab.com"
 
-#we need to save the users ID. Using the username won't work.
-ID=$(curl -s --header "PRIVATE-TOKEN: $TOKEN" -H "Content-Type: application/json" "$URL/api/v4/users?username=$USER"| jq -r '.[].id')
+#Checking if you provide an username
+if [[ -z $1 ]]; then
+    echo ""
+    echo " .. Gief me username plz 🤌🏼🤌🏼"
+    echo ""
+else
+    #we need to save the users ID. Using the username won't work.
+    ID=$(curl -s --header "PRIVATE-TOKEN: $TOKEN" -H "Content-Type: application/json" "$URL/api/v4/users?username=$USER"| jq -r '.[].id')
 
-#The command that actually resets the 2fa for an user id
-curl -s --request PATCH --header "PRIVATE-TOKEN: $TOKEN" "$URL/api/v4/users/$ID/disable_two_factor"
+    #The command that actually resets the 2fa with an user id
+    curl -s --request PATCH --header "PRIVATE-TOKEN: $TOKEN" "$URL/api/v4/users/$ID/disable_two_factor"
 
-echo ""
-echo ".. 2fa for '$1' has been reset."
+    echo ""
+    echo ".. 2fa for '$1' has been reset👍🏼"
+fi
